8d5f69f609bbfeffc529a36bc6f2ac173bef3fd8
Kseniia Chepur <kseniia.chepur@gmail.com>
2025-02-07 21:39:16 +0200

    add styles for layout component, add Noto Sans Mono font family

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIconModule],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
